6f02769ab62352c864f58642873389421cd6368f
Anderson Lima <67929816+A-Infor@users.noreply.github.com>
2025-10-28 06:52:44 -0300

    Improvements
    Making code more elegant.

--- raspberry_pi_magazine_downloader.sh
+++ raspberry_pi_magazine_downloader.sh
@@ -2,12 +2,12 @@
 declare START_ISSUE=158
 declare FINAL_ISSUE=1
 declare URL_ROOT='https://magazine.raspberrypi.com'
-declare MATCH_PATTERN='<a class="link" href="'
+declare MATCH_PATTERN='<a class=\"link\" href=\".*\.pdf\"'
 
 for issue in $(seq $START_ISSUE -1 $FINAL_ISSUE)
 do
-	download_page=$(wget -qO- "$URL_ROOT/issues/$issue/pdf/download")
-	url_suffix=$(echo "$download_page" | grep -F "$MATCH_PATTERN" | cut -d'"' -f4)
+	download_page=$(wget -qO- $URL_ROOT/issues/$issue/pdf/download)
+	url_suffix=$(grep -oe "$MATCH_PATTERN" <<< "$download_page" | cut -d'"' -f4)
 	pdf_url="$URL_ROOT$url_suffix"
 	wget -q -N --show-progress "$pdf_url"
 done
